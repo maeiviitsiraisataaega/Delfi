39d37036d1bb9e87f39173b5a6c2487e60101cba
Oliver <83928889+maeiviitsiraisataaega@users.noreply.github.com>
2022-06-02 03:29:55 +0300

    Update Delfi Dark Mode.user.js

--- Delfi Dark Mode.user.js
+++ Delfi Dark Mode.user.js
@@ -1,17 +1,41 @@
-// ==UserScript==
+/// ==UserScript==
 // @name         Delfi Dark Mode
 // @namespace    https://github.com/maeiviitsiraisataaega/Delfi/
 // @version      0.1
 // @description  Dark mode Delfi jaoks
 // @author       AHVIPEA
 // @match        homepage
-// @match        https://www.delfi.ee*
-// @resource   IMPORTED_CSS https://github.com/maeiviitsiraisataaega/Delfi/css.css
-// @grant      GM_getResourceText
-// @grant      GM_addStyle
+// @match        https://www.delfi.ee/*
+// @resource     REMOTE_CSS https://raw.githubusercontent.com/maeiviitsiraisataaega/Delfi/main/css.css?version=5
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
 // ==/UserScript==
+
+function addGlobalStyle(css) {
+	var head, style;
+	head = document.getElementsByTagName('head')[0];
+	if (!head) { return; }
+	style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = css;
+	head.appendChild(style);
+}
+
+addGlobalStyle('body, .C-block-type-11, .G-col { background-color: #121212 !important; color: #FFFFFF !important; }');
+addGlobalStyle('h1, h2, h3, h4, h5, .C-block-type-social-text, a, .C-headline-title, .PILETITASKU-ticket__title, .PILETITASKU-ticket__date, #LB24, .lb24 { color: #FFFFFF !important; }');
+addGlobalStyle('.C-headline-title__share, .C-comment-count { display: none !important; }');
+addGlobalStyle('.WEATHER-location-search { background: #121212 !important; color: #FFFFFF !important; }');
+
 
 (function() {
     'use strict';
-    const my_css = GM_getResourceText("IMPORTED_CSS");
-    GM_addStyle(my_css);
+    // Load remote CSS
+    // @see https://github.com/Tampermonkey/tampermonkey/issues/835
+    const myCss = GM_getResourceText("REMOTE_CSS");
+    GM_addStyle(myCss);
+
+
+
+})();
+
+
